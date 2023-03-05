1c17caa3334fe526506ed6dd7b548119d5144c32
Josh <im.wangzhenxi@gmail.com>
2023-03-05 15:19:38 +0800

    auto commit

--- packages/cms/src/pages/$/index.jsx
+++ packages/cms/src/pages/$/index.jsx
@@ -10,14 +10,9 @@ import {
   Row as NextRow,
   Col as NextCol,
   P as NextP,
-  PageHeader as NextPageHeader,
 } from '@alifd/pro-layout/lib/index.js';
 
 import { NextText } from '@alilc/lowcode-materials/lib/index.js';
-
-import { createFetchHandler as __$$createFetchRequestHandler } from '@alilc/lowcode-datasource-fetch-handler';
-
-import { create as __$$createDataSourceEngine } from '@alilc/lowcode-datasource-engine/runtime';
 
 import utils, { RefsManager } from '../../utils';
 
@@ -31,20 +26,6 @@ import '@alilc/lowcode-materials/lib/style';
 
 class $$Page extends React.Component {
   _context = this;
-
-  _dataSourceConfig = this._defineDataSourceConfig();
-  _dataSourceEngine = __$$createDataSourceEngine(this._dataSourceConfig, this, {
-    runtimeConfig: true,
-    requestHandlersMap: { fetch: __$$createFetchRequestHandler() },
-  });
-
-  get dataSourceMap() {
-    return this._dataSourceEngine.dataSourceMap || {};
-  }
-
-  reloadDataSource = async () => {
-    await this._dataSourceEngine.reloadDataSource();
-  };
 
   constructor(props, context) {
     super(props);
@@ -65,35 +46,6 @@ class $$Page extends React.Component {
   $$ = (refName) => {
     return this._refsManager.getAll(refName);
   };
-
-  _defineDataSourceConfig() {
-    const _this = this;
-    return {
-      list: [
-        {
-          type: 'fetch',
-          isInit: function () {
-            return true;
-          },
-          options: function () {
-            return {
-              params: {},
-              method: 'GET',
-              isCors: true,
-              timeout: 5000,
-              headers: {},
-              uri: 'mock/info.json',
-            };
-          },
-          id: 'info',
-          shouldFetch: function () {
-            console.log('should fetch.....');
-            return true;
-          },
-        },
-      ],
-    };
-  }
 
   componentWillUnmount() {
     console.log('will unmount');
@@ -135,8 +87,6 @@ class $$Page extends React.Component {
   }
 
   componentDidMount() {
-    this._dataSourceEngine.reloadDataSource();
-
     console.log('did mount');
   }
 
@@ -158,10 +108,42 @@ class $$Page extends React.Component {
           aside={false}
           placeholderStyle={{ gridRowEnd: 'span 1', gridColumnEnd: 'span 12' }}
           headerProps={{ background: 'surface' }}
-          header={
-            <NextPageHeader
-              style={{ background: '#ffffff', padding: '' }}
-              ref={this._refsManager.linkRef('nextpageheader-9a055598')}
+          header=""
+          isTab={false}
+          contentAlignCenter={false}
+          contentProps={{ style: { background: 'rgba(255,255,255,0)' } }}
+          navProps={{ width: 200 }}
+          asideProps={{ width: 200 }}
+          background="lining"
+          ref={this._refsManager.linkRef('nextpage-3225e27a')}
+        >
+          <NextBlock
+            placeholderStyle={{ height: '100%' }}
+            noPadding={false}
+            noBorder={false}
+            title=""
+            rowGap={20}
+            colGap={20}
+            background="surface"
+            layoutmode="O"
+            strict={true}
+            colSpan={12}
+            rowSpan={1}
+            mode="transparent"
+            childTotalColumns={12}
+          >
+            <NextBlockCell
+              colSpan={12}
+              rowSpan={1}
+              mode="procard"
+              isAutoContainer={true}
+              title=""
+              hasDivider={true}
+              loading={false}
+              hasCollapse={false}
+              text={true}
+              operations={[]}
+              isFillContainer={true}
             >
               <NextRowColContainer rowGap={20} colGap={20}>
                 <NextRow>
@@ -175,107 +157,21 @@ class $$Page extends React.Component {
                       prefix=""
                       full={false}
                       flex={false}
-                      ref={this._refsManager.linkRef('nextp-c48185b9')}
-                    >
-                      <NextText
-                        type="h1"
-                        mark={false}
-                        code={false}
-                        delete={false}
-                        underline={false}
-                        strong={false}
-                        prefix=""
-                        classname=""
-                      >
-                        标题
-                      </NextText>
-                    </NextP>
-                  </NextCol>
-                </NextRow>
-              </NextRowColContainer>
-            </NextPageHeader>
-          }
-          isTab={false}
-          contentAlignCenter={false}
-          contentProps={{ style: { background: 'rgba(255,255,255,0)' } }}
-          navProps={{ width: 200 }}
-          asideProps={{ width: 200 }}
-          background="lining"
-          ref={this._refsManager.linkRef('nextpage-3225e27a')}
-        >
-          <NextBlock
-            placeholderStyle={{ height: '100%' }}
-            noPadding={false}
-            noBorder={false}
-            title="区域标题"
-            rowGap={20}
-            colGap={20}
-            background="surface"
-            layoutmode="O"
-            strict={true}
-            colSpan={12}
-            rowSpan={1}
-            mode="transparent"
-            childTotalColumns={12}
-            ref={this._refsManager.linkRef('nextblock-7b032620')}
-          >
-            <NextBlockCell
-              colSpan={12}
-              rowSpan={1}
-              mode="procard"
-              isAutoContainer={true}
-              title="多语言示例"
-              hasDivider={true}
-              loading={false}
-              bodyPadding=""
-              hasCollapse={false}
-              text={true}
-              visibleButtonCount={3}
-              isFillContainer={true}
-              operationConfig={{ align: 'center' }}
-              operations={[]}
-              ref={this._refsManager.linkRef('nextblockcell-c210269c')}
-            >
-              <NextRowColContainer
-                rowGap={20}
-                colGap={20}
-                ref={this._refsManager.linkRef('nextrowcolcontainer-626dc975')}
-              >
-                <NextRow>
-                  <NextCol colSpan={1} justifyContent="flex-start">
-                    <NextP
-                      wrap={false}
-                      type="body2"
-                      verAlign="middle"
-                      textSpacing={true}
-                      align="left"
-                      prefix=""
-                      full={false}
-                      flex={false}
                     >
                       <NextText
                         type="h5"
-                        prefix=""
-                        classname=""
                         mark={false}
                         code={false}
                         delete={false}
                         underline={false}
                         strong={false}
-                      >
-                        {__$$eval(() => this.getHelloWorldText())}
-                      </NextText>
-                      <NextText
-                        type="h5"
                         prefix=""
                         classname=""
-                        mark={false}
-                        code={false}
-                        delete={false}
-                        underline={false}
-                        strong={false}
                       >
-                        {__$$eval(() => this.getHelloWorldText2())}
+                        {__$$eval(
+                          () =>
+                            this.getHelloWorldText() + this.getHelloWorldText2()
+                        )}
                       </NextText>
                     </NextP>
                   </NextCol>
